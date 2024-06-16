f8ae74da89e5699dd3875bb7f6a0583f83710759
Giovanni Ricca <giovanniricca@proton.me>
2024-06-16 16:36:20 +0200

    zircon: Patch libsource to load the old graphicsbuffer symbol
    Change-Id: Ic8f1880379bf7adf07193842a710a104e434d470

--- device.mk
+++ device.mk
@@ -210,6 +210,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_PACKAGES += \
+    libui_shim
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib64/libsource.so)
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
         odm/lib*/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
