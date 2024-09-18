a98a06beb33f92491df5d342e6d1cbab8bb869e6
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-19 00:52:40 +0530

    zircon: Setup recovery
    Change-Id: I8d31ac2029ab838ba455b360e4e13fdd9dcdcfac

--- device.mk
+++ device.mk
@@ -7,8 +7,17 @@
 # Generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Rootdir
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.mt6886:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6886 \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6886.rc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/init.recovery.mt6886.rc
 
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 33
