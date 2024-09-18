aabb8d304c2b5bbf8d128801c329d3cd5349505f
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-19 01:01:11 +0530

    zircon: Setup kernel params
    Change-Id: I79b9ed0cf342e9e1df115251aef222d5c9777436

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
