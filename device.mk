2c2fcf091a85300dded930cba690136b359b28bd
AdarshGrewal <adarshgrewal@gmail.com>
2024-10-23 16:52:53 +0530

    zircon: Include hardware/mediatek namespace
    Change-Id: I6905eaf7a60ad45906bbd8138fa23a312eb19e89

--- device.mk
+++ device.mk
@@ -319,7 +319,8 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Telephony
 PRODUCT_COPY_FILES += \
