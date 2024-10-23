c00229b79b9f44c8ddcaccbe42aa125e9a946a47
AdarshGrewal <adarshgrewal@gmail.com>
2024-10-23 19:56:52 +0530

    zircon: Inherit hardware/xiaomi namespace
    Change-Id: I86bb33fa5369d2df77ebb9ee9171b73d4bf76b73

--- device.mk
+++ device.mk
@@ -331,7 +331,8 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_COPY_FILES += \
