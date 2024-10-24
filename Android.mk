99253fbf3ccf18ac5681ae905dee4ffeed864427
AdarshGreal <adarshgrewal@gmail.com>
2024-10-24 20:45:49 +0530

    zircon: Declare all symlinks during extraction
    vendor/bin/hw//vendor/bin/hw/mt6886/android.hardware.graphics.allocator@4.0-service-mediatek.mt6886
    vendor/bin/hw//vendor/bin/hw/mt6886/arm.mali.platform-service.mediatek.mt6886
    vendor/etc//mnt/vendor/mdota
    vendor/lib/hw/libSoftGatekeeper.so
    vendor/lib64/hw/libSoftGatekeeper.so

--- Android.mk
+++ Android.mk
@@ -7,14 +7,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),zircon)
-include $(LOCAL_PATH)/vendor-symlinks.mk
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-GMA_CUSTOM_SYMLINK := $(TARGET_OUT_VENDOR)/etc/camera/gma_custom.xml
-$(GMA_CUSTOM_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Symlink vendor gma_custom.txt to gma_custom.xml"
-	$(hide) ln -sf /vendor/etc/camera/gma_custom.txt $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(GMA_CUSTOM_SYMLINK)
-
 endif
