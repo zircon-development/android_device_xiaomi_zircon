4e13b1aa7808a7acf37fb6766288785b283108f5
Aaron Kling <webgeek1234@gmail.com>
2024-12-29 15:06:02 -0600

    zircon: Patch composer for forked composer utils
    Change-Id: Ic1eda5eaa81cb21ad95a285e1d5a60ea7df6832a

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libshim_sink.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
+    'vendor/bin/hw/android.hardware.graphics.composer@3.1-service': blob_fixup()
+        .replace_needed('android.hardware.graphics.composer@2.1-resources.so', 'android.hardware.graphics.composer@2.1-resources-v34.so'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.mitee': blob_fixup()
