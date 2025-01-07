52e7650ea1cea249a9ad0102b7e5df66746d52ee
AdarshGrewal <adarshgrewal@gmail.com>
2025-01-07 22:22:26 +0530

    zircon: Update from OS2.0.4.0.VNOMIXM
    Change-Id: Iba47358ddd9697aa60a37088c99a2dc336f97ac4

--- lineage_zircon.mk
+++ lineage_zircon.mk
@@ -24,8 +24,8 @@ PRODUCT_SYSTEM_NAME := zircon_global
 PRODUCT_SYSTEM_DEVICE := zircon
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="zircon_global-user 14 UP1A.231005.007 V816.0.9.0.UNOMIXM release-keys" \
-    BuildFingerprint=Redmi/zircon_global/zircon:14/UP1A.231005.007/V816.0.9.0.UNOMIXM:user/release-keys \
+    BuildDesc="zircon_global-user 15 AP3A.240905.015 OS2.0.4.0.VNOMIXM release-keys" \
+    BuildFingerprint=Redmi/zircon_global/zircon:15/AP3A.240905.015.A2/OS2.0.4.0.VNOMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
